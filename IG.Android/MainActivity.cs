176aa07b4231bf03f4a1c4f8c787b9beaef67ccd
Andres Zapata <andres.zapata@doctus.com.co>
2020-07-01 20:01:15 -0500

    Splash

--- IG.Android/MainActivity.cs
+++ IG.Android/MainActivity.cs
@@ -6,7 +6,7 @@
     using Android.OS;
     using Android.Runtime;
 
-    [Activity(Label = "IG", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "IG", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ IG.Android/SplashActivity.cs
@@ -0,0 +1,22 @@
+﻿namespace IG.Droid
+{
+    using Android.App;
+    using Android.Content;
+    using Android.Content.PM;
+    using Android.OS;
+    
+    [Activity(Label = "IG", Icon = "@mipmap/icon", Theme = "@style/Theme.splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            var intent = new Intent(this, typeof(MainActivity));
+            if (Intent.Extras != null)
+                intent.PutExtras(Intent.Extras);
+            StartActivity(intent);
+        }
+    }
+}
